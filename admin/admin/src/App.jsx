0e12b8cd92e03d0a65359f695095d51145ca29e7
VarunSharma35 <varunsharma24025@gmail.com>
2025-02-22 22:24:21 +0530

    Update App.jsx

--- admin/admin/src/App.jsx
+++ admin/admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "https://food-del-backend-6n2n.onrender.com"
+  const url = "http://localhost:4000"
 
   return (
     <div>
